7bec62b1b9852316a868537dbb6ed1a039f4a34c
Ryan Northey <ryan@3ca.org.uk>
2012-03-19 12:06:08 +0000

    paths

--- bit/recipe/android_sdk/__init__.py
+++ bit/recipe/android_sdk/__init__.py
@@ -8,21 +8,22 @@ class Recipe:
     def __init__(self, buildout, name, options):
         self.options = options
         self.logger = logging.getLogger(name)
+        self.name = name
 
     def _update(self):
-        apis = self.options['apis'].split('\n')        
         install = pkg_resources.resource_filename(__name__, 'install_android_sdk')
         path = os.getcwd()
         target_install = os.path.join(path,'bin','install_android_sdk')
+        part = os.path.join(path,'parts',self.name)
         if os.path.exists(target_install)
             os.unlink(target_install)
-        open(target_install,'w').write('#/bin/bash\nDK=%s\nAPIS=%s\n' %(self.options[dk],tuple(apis))+open(install).read())
+        open(target_install,'w').write('#/bin/bash\nSDK=%s\nAPIS=%s\n' %(self.options['sdk'],tuple(apis))+open(install).read())
         commands.getoutput('chmod +x %s' %target_install)
-        open(os.path.join('bin', 'android_sdk'),'w').write('BUILDOUT=`pwd`\nexport ANDROIDAPI=8\nexport ANDROIDSDK=$BUILDOUT/parts/android-sdk\nexport PATH=$ANDROIDSDK:$PATH') 
+        open(os.path.join('bin', 'android_sdk'),'w').write('BUILDOUT=%s\nexport ANDROIDAPI=%s\nexport ANDROIDSDK=%s\nexport PATH=$ANDROIDSDK:$PATH'%(path,apis[0],part) 
 
     def install(self):
         self._update()
-        return ['bin/install_android_sdk',]
+        return ['bin/%s'self.name,]
 
     def update(self):
         self._update()
