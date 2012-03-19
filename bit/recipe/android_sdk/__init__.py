331602d2408dad110a1099128303a8da5ff7e533
Ryan Northey <ryan@3ca.org.uk>
2012-03-19 18:00:02 +0000

    pep8

--- bit/recipe/android_sdk/__init__.py
+++ bit/recipe/android_sdk/__init__.py
@@ -4,28 +4,38 @@ import shutil
 import pkg_resources
 import commands
 
+
 class Recipe:
+
     def __init__(self, buildout, name, options):
         self.options = options
         self.logger = logging.getLogger(name)
         self.name = name
 
     def _update(self):
-        install = pkg_resources.resource_filename(__name__, 'install_android_sdk')
+        install = pkg_resources.resource_filename(
+            __name__, 'install_android_sdk')
         path = os.getcwd()
-        target_install = os.path.join(path,'bin',self.name)
-        part = os.path.join(path,'parts',self.name)
+        target_install = os.path.join(path, 'bin', self.name)
+        part = os.path.join(path, 'parts', self.name)
         apis = self.options['apis']
         if os.path.exists(target_install):
             os.unlink(target_install)
-        open(target_install,'w').write('#/bin/bash\nBUILDOUT=%s\nSDK=%s\nAPIS=%s\nSDKDIR=%s\nPARTNAME=%s\n' %(path,self.options['sdk'],tuple(apis),part,self.name)+open(install).read())
-        commands.getoutput('chmod +x %s' %target_install)
-        
-        #open(os.path.join('bin', 'android_sdk'),'w').write('BUILDOUT=%s\nexport ANDROIDAPI=%s\nexport ANDROIDSDK=%s\nexport PATH=$ANDROIDSDK:$PATH'%(path,apis[0],part))
+        env_vars = {'BUILDOUT': path,
+                    'SDK': self.options['sdk'],
+                    'APIS': tuple(apis),
+                    'SDKDIR': part,
+                    'PARTNAME': self.name,
+                    }
+        bash = '#/bin/bash\n'
+        bash += '\n'.join(['%s=%s' % (k, v)
+                          for k, v in env_vars.items()])
+        open(target_install, 'w').write(bash + open(install).read())
+        commands.getoutput('chmod +x %s' % target_install)
 
     def install(self):
         self._update()
-        return ['bin/%s'%self.name,]
+        return ['bin/%s' % self.name, ]
 
     def update(self):
         self._update()
--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 name = "bit.recipe.android_sdk"
-version = '0.0.1'
+version = '0.0.2'
 setup(
-    name = name,
-    version = version,
-    author = "Ryan Northey",
-    author_email = "ryan@3ca.org.uk",
-    description = "bit foundation android_sdk zc.buildout recipe",
-    license = "GPL",
-    keywords = "buildout",
-    url='http://www.python.org/pypi/'+name,
+    name=name,
+    version=version,
+    author="Ryan Northey",
+    author_email="ryan@3ca.org.uk",
+    description="bit foundation android_sdk zc.buildout recipe",
+    license="GPL",
+    keywords="buildout",
+    url='http://www.python.org/pypi/' + name,
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['bit','bit.recipe'],
+    namespace_packages=['bit', 'bit.recipe'],
     include_package_data=True,
     zip_safe=False,
-    install_requires = ['zc.buildout', 'setuptools'],
-    entry_points = {'zc.buildout':
-                    ['default = %s:Recipe' % name]},
+    install_requires=['zc.buildout', 'setuptools'],
+    entry_points={'zc.buildout':
+                    ['default=%s:Recipe' % name]},
     )
